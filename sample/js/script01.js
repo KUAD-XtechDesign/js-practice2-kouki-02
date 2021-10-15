0f298b65e170dcf956c970a39ae977d13c0e058e
kouki-02 <k-doi2270@st.kyoto-art.ac.jp>
2021-10-15 15:13:55 +0900

    changed_practice2

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,17 @@
 $(function(){
 
+  let txt1 = "おはようございます！"
+  let txt2 = "おげんきですか？";
+  let num1 = 20;
+  let num2 = 32;
 
+  function sayHello(){
+      $("#content").text("こんにちは！");
+  }
 
+  function saySomething(txt){
+      $("#content").text(txt);
+  }
+  
 
 })
